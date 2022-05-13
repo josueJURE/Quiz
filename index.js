d95a3c39377ab7e9308de85e6b507352bd3b20a9
josueJURE <josue.jure@gmail.com>
2022-05-13 19:45:58 +0100

    attach event listeners to 'select' and 'button' elements

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const select = document.querySelector("select");
+const button = document.querySelector("button");
+const intro = document.querySelector(".intro")
+
+select.addEventListener("change", displayButton);
+button.addEventListener("click", startQuiz);
+
+function displayButton() {
+  button.style.display = "block"
+}
+
+function startQuiz() {
+  button.style.display = "none"
+  intro.style.display = "none"
+
+}
