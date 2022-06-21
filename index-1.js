e4ccbee5c616689ea42eec87ab0b7873f0490922
josueJURE <josue.jure@gmail.com>
2022-06-21 10:59:00 +0100

    create userRanking() using localStorage

--- index-1.js
+++ index-1.js
@@ -19,12 +19,70 @@ const questionAnswered = document.querySelector(".questionAnswered");
 const btn_1 = document.querySelector(".btn_1");
 const landingPage = document.querySelector(".landingPage");
 const input = document.querySelector("input");
-// const userName = input.value;
+let [ratio, counter, count, clock, score, timeUp] = [0, 0, 0, 0, 0, 10];
 
+// let score = count/quizLength;
 
-let [ratio, counter, count, clock, timeUp] = [0, 0, 0, 0, 10];
+console.log(input.value)
 
-import {japaneseAnimeJapanese, japaneseAnimeEnglish} from "./index-2.js"
+let japaneseAnimeJapanese = [
+{
+  name: "ドラゴンボール",
+  picture: "https://dbgbh.bn-ent.net/assets/img/news/news_thumb_kv.png"
+},
+{
+  name: "進撃の巨人",
+  picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJYjy_bS8t3ScWyG7q94fIltnar3ChaOHmGA&usqp=CAU"
+},
+{
+  name: "ナルト",
+  picture: "https://res.cloudinary.com/jerrick/image/upload/v1616592065/605b3cc118e784001e22da0d.jpg"
+},
+{
+  name: "鬼滅の刃",
+  picture: "https://cdn.vox-cdn.com/thumbor/gcVHhhZ4VwVswvbDPvI-RfQ7ECQ=/1400x1050/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/19721018/Tanjiro__Demon_Slayer_.png"
+},
+{
+  name: "攻殻機動隊",
+  picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8VBbI5HMki5cmjP_Gq0TdyA6VZn_0_fmkhg&usqp=CAU"
+}
+]
+
+let japaneseAnimeEnglish = [
+{
+  name: "Dragon ball",
+  picture: "https://dbgbh.bn-ent.net/assets/img/news/news_thumb_kv.png"
+},
+{
+  name: "Attack On Titans",
+  picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJYjy_bS8t3ScWyG7q94fIltnar3ChaOHmGA&usqp=CAU"
+},
+{
+  name: "naruto",
+  picture: "https://res.cloudinary.com/jerrick/image/upload/v1616592065/605b3cc118e784001e22da0d.jpg"
+},
+{
+  name: "Demon Slayer",
+  picture: "https://cdn.vox-cdn.com/thumbor/gcVHhhZ4VwVswvbDPvI-RfQ7ECQ=/1400x1050/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/19721018/Tanjiro__Demon_Slayer_.png"
+},
+{
+  name: "Ghost in the shell",
+  picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8VBbI5HMki5cmjP_Gq0TdyA6VZn_0_fmkhg&usqp=CAU"
+}
+]
+
+// export {japaneseAnimeJapanese, japaneseAnimeEnglish};
+// import {japaneseAnimeJapanese, japaneseAnimeEnglish} from "./index-2.js"
+
+const ranking = JSON.parse(localStorage.getItem("userObject")) || [];
+
+function userRanking() {
+  ranking.push({
+    userName: input.value,
+    userScore: (score / quizLength) * 100,
+  })
+  localStorage.setItem("userObject", JSON.stringify(ranking))
+}
 
 btn_1.addEventListener("click", slideFunction);
 btn_2.addEventListener("click", startQuiz);
@@ -40,7 +98,6 @@ function quizClock() {
   if(timeUp > 0) {
     timeUp--;
     ratio++;
-
   } else {
     timeUp = 10;
     ratio = 0;
@@ -59,7 +116,8 @@ function checkUserAnswer(e) {
   let answer = e.target;
   if(answer.innerHTML === japaneseAnimeEnglish[counter].name) {
     correctAnswer();
-    count++
+    count++;
+    score++;
   } else {
     wrongAnswer();
   }
@@ -91,18 +149,14 @@ function trackQuestion() {
     questionAnswered.innerHTML = `question ${counter+1} of ${quizLength}`;
   }
 }
-
+//
 function displayFinalScore() {
-  let score = count/quizLength;
-  let userName = input.value;
-  localStorage.setItem("name", userName);
-  localStorage.setItem("score", score*100)
   quiz.style.display = "none";
   finalScore.style.display = "flex";
-  bubble.innerHTML = `<h1>Well done ${localStorage.name} Your final score is ${(score)*100}%</h1>`
+  bubble.innerHTML = `<h1>Well done ${input.value} Your final score is ${(score/quizLength)*100}%</h1>`
   gif.innerHTML = `<img src="images/dragon-ball-z-goku.gif" alt="">`
   document.body.style.backgroundColor = "#368dda";
-  console.log(localStorage)
+  userRanking()
 }
 
 
@@ -137,19 +191,16 @@ function displayButton() {
   introMessage.style.display = "none";
 }
 
-
-// window.addEventListener("DOMContentLoaded", startQuiz, false);
-
-function startQuiz() {
-    quiz.style.display = "flex";
-    container.style.display = "none";
-    intro.style.display = "none";
-    displayUniqueChoices();
-    userChoice();
-    displayRoundElements();
-    trackQuestion()
-    setInterval(quizClock, 1000);
-}
+  function startQuiz() {
+      quiz.style.display = "flex";
+      container.style.display = "none";
+      intro.style.display = "none";
+      displayUniqueChoices();
+      userChoice();
+      displayRoundElements();
+      trackQuestion()
+      setInterval(quizClock, 1000);
+  }
 
 body.addEventListener("click", function(e) {
   if(input.value === "" && e.key !== "Enter") return;
