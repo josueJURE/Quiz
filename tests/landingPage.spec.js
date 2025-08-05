f1c2e37165fe2c95c4b0227de3ed9124c41232c2
josueJURE <josue.jure@gmail.com>
2025-08-05 11:58:25 +0100

    (feat:playwright) write 1st test

---
+++ tests/landingPage.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+
+test.describe("Landing page", () => {
+    test("user should see Anime Quiz Challenge", async ({page}) => {
+        await page.goto('http://127.0.0.1:5500/index.html'); // Make sure you navigate to the page first
+         await expect(page.getByText("Anime Quiz Challenge")).toBeVisible()
+       
+
+    })
+})
+
